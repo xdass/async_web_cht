83c5bb2f6d90fea9f8e51a772b05a39a2ffb234e
dass <420600base>
2019-12-17 16:33:55 +0500

    Добавлены настройки TCP KEEPALIVE для отладки разрыва соединения

---
+++ util.py
@@ -0,0 +1,18 @@
+import socket
+
+
+def set_keepalive_linux(sock, after_idle_sec=1, interval_sec=3, max_fails=3):
+    """Set TCP keepalive on an open socket.
+
+    It activates after 1 second (after_idle_sec) of idleness,
+    then sends a keepalive ping once every 3 seconds (interval_sec),
+    and closes the connection after 5 failed ping (max_fails), or 15 seconds
+    """
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, after_idle_sec)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, interval_sec)
+    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, max_fails)
+
+
+def set_keepalive_win(sock):
+    sock.ioctl(socket.SIO_KEEPALIVE_VALS, (1, 10000, 3000))
